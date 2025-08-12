ab537a6551111a6c651eded5733cb9dc34438897
igorgcf <igorTecnologia@outlook.com>
2025-08-11 21:46:44 -0300

    Model Person Abstract (Roberto)

--- src/models/Person.java
+++ src/models/Person.java
@@ -1,4 +1,52 @@
 package models;
 
 public abstract class Person {
+    private String nome;
+    private int idade;
+    private String nacionalidade;
+
+    public Person(String nome, int idade, String nacionalidade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.nacionalidade = nacionalidade;
+    }
+
+    public Person() {
+
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = "Roberto";
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    // Método comum
+    public void exibirInfo() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Nacionalidade: " + nacionalidade);
+    }
+
+    // Método abstrato — cada tipo de Persona define seu papel
+    public abstract void descreverPapel();
 }
