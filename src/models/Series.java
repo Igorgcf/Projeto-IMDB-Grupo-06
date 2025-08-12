9ad4993bfa81b146f6dc9c75947f55d2e6154b69
GOS-briel <gabrieldeoliveiragos2@gmail.com>
2025-08-11 23:06:09 -0300

    Model Series (Gabriel)

--- src/models/Series.java
+++ src/models/Series.java
@@ -1,4 +1,22 @@
 package models;
 
-public class Series {
+public class Series extends ItemCatalog  {
+  private int seasons;
+
+    public Series(String name,  String creationDate, int seasons, String description, double budget) {
+        super(name, creationDate, description, budget);
+
+    this.seasons = seasons;
+
+    }
+
+
+    public void setSeasons(int seasons) {
+    }
+
+    public void getSeasons(int seasons) {
+
+    }
+
+
 }
