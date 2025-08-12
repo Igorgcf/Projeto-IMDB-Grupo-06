27237d330cc1b767ef6739e86bbb4a8c8347c98e
igorgcf <igorTecnologia@outlook.com>
2025-08-11 21:45:58 -0300

    Model Actor (Roberto)

--- src/models/Actor.java
+++ src/models/Actor.java
@@ -1,4 +1,14 @@
 package models;
 
-public class Actor {
+public class Actor extends Person {
+    private final String personagem;
+
+    public Actor(String nome, int idade, String nacionalidade, String personagem) {
+        super(nome, idade, nacionalidade);
+        this.personagem = personagem;
+    }
+
+    public void descreverPapel() {
+        System.out.println(getNome() + " interpreta o personagem: " + personagem);
+    }
 }
